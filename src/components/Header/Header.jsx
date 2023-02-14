6452fecab4e14315ebc7c72206e68614460c1e3a
Ivana Braz <braz.ivana@gmail.com>
2023-02-13 21:29:43 -0300

    Fixing design

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+            <div className="w-full h-[100vh] bg-purple-3 flex items-center justify-center text-center">
+                <p className="text-white text-5xl">
+                    Śpiewnik
+                </p>
+            </div>
+    )
+}
+
+export default Header;
--- src/components/Spiewnik/SpiewnikLyrics.jsx
+++ src/components/Spiewnik/SpiewnikLyrics.jsx
@@ -4,14 +4,12 @@ const SpiewnikLyrics = ({ piosenka }) => {
 
     return (
         <>
-        <div className={`bg-black absolute z-100 flex flex-col top-0 items-center text-center w-full h-screen
-            text-xl p-10`}>
-            <div className='w-[50%]'>
-                <h2 className="text-2xl text-white mb-10">
+        <div className={`w-full h-screen bg-neutral-4 flex justify-center`}>
+            <div className='py-20'>
+                <h2 className="text-4xl text-white mb-10 mt-10">
                     {piosenka.title}
                 </h2>
                 <p className="text-base text-white" dangerouslySetInnerHTML={{ __html: piosenka.lyrics }}>
-                    
                 </p>
             </div>
         </div>
